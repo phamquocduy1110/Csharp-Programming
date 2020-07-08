54907764c8f3724d93c0ac85d17050ff73d1b498
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-07-08 20:21:23 +0700

    update

---
+++ Student Management/WindowsFormsApplication1/WindowsFormsApplication1/MemberView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class MemberView
+    {
+        public int id { get; set; }
+        public string Fullname { get; set; }
+        public string Gender { get; set; }
+        public string Birthday { get; set; }
+        public string Status { get; set; }
+        public string Hometowm { get; set; }
+        public string Currentliving { get; set; }
+        public string Occupation { get; set; }
+        public string Father { get; set; }
+        public string Mother { get; set; }
+        public string Spouse{ get; set; } 
+       
+        public MemberView(Member member)
+        {
+            this.id = member.id;
+            this.Fullname = member.Fullname;
+            this.Gender = member.Gender == true ? "Nam" : "Nu";
+            if (member.Birthday != null)
+                this.Birthday = string.Format("{0}/{1}/{2}",
+                    member.Birthday.Value.Day,
+                    member.Birthday.Value.Month,
+                    member.Birthday.Value.Year
+                    );
+            if (member.Status.HasValue)
+                this.Status = member.Status.Value == true ? "Living" : "Deceased";
+            var db = new OOPCSEntities();
+            if(member.Father_id.HasValue)
+            {
+                var father = db.Members.Find(member.Father_id.Value);
+                this.Father = father.Fullname;
+            }
+
+            if (member.Mother_id.HasValue)
+            {
+                var mother = db.Members.Find(member.Mother_id.Value);
+                this.Mother = mother.Fullname;
+            }
+
+            if (member.Spouse_id.HasValue)
+            {
+                var spouse = db.Members.Find(member.Spouse_id.Value);
+                this.Spouse = spouse.Fullname;
+            }
+            this.Hometowm = member.Hometowm;
+            this.Currentliving = member.Currentliving;
+            this.Occupation = member.Occupation;
+
+        }
+    }
+}
---
+++ Student Management/WindowsFormsApplication1/WindowsFormsApplication1/ViewAllMembersForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ViewAllMembersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdMembers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdMembers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdMembers
+            // 
+            this.grdMembers.AllowUserToAddRows = false;
+            this.grdMembers.AllowUserToDeleteRows = false;
+            this.grdMembers.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.grdMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdMembers.GridColor = System.Drawing.SystemColors.Control;
+            this.grdMembers.Location = new System.Drawing.Point(0, 0);
+            this.grdMembers.Name = "grdMembers";
+            this.grdMembers.ReadOnly = true;
+            this.grdMembers.Size = new System.Drawing.Size(504, 249);
+            this.grdMembers.TabIndex = 0;
+            this.grdMembers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ViewAllMembersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 261);
+            this.Controls.Add(this.grdMembers);
+            this.Name = "ViewAllMembersForm";
+            this.Text = "ViewAllMembersForm";
+            ((System.ComponentModel.ISupportInitialize)(this.grdMembers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdMembers;
+
+    }
+}
